c5f9030402e0c2f7112d9a5bd23ae23869708a0d
JoseAntonioPerezMiguel <jose-antonio.perez-miguel@capgemini.com>
2024-06-18 16:02:33 +0200

    Record and observer pattern

--- demo/src/main/java/com/example/ioc/MyConfig.java
+++ demo/src/main/java/com/example/ioc/MyConfig.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.event.EventListener;
+
+import com.example.ioc.SaludaImpl.SaludaEvent;
 
 @Configuration
 public class MyConfig {
@@ -22,4 +25,8 @@ public class MyConfig {
 		return new EntornoImpl(contInt);
 	}
 
+	@EventListener
+	void trataEvento(SaludaImpl.SaludaEvent ev) {
+		System.err.println("Evento -> " + ev.tipo() + " -> " + ev.destinatario());
+	}
 }
--- demo/src/main/java/com/example/ioc/SaludaImpl.java
+++ demo/src/main/java/com/example/ioc/SaludaImpl.java
@@ -3,6 +3,7 @@ package com.example.ioc;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -15,23 +16,37 @@ import lombok.NonNull;
 @Profile("es")
 public class SaludaImpl implements Saluda {
 	
-	Entorno entorno;
+	public static record SaludaEvent(String tipo, String destinatario) {	}
+
+	private ApplicationEventPublisher publisher;
 	
-	public SaludaImpl(Entorno entorno) {
+	Entorno entorno;
+
+	public SaludaImpl(Entorno entorno, ApplicationEventPublisher publisher) {
 		this.entorno = entorno;
+		this.publisher = publisher;
+	}
+	
+	protected void onSaluda(@NonNull String tipo, @NonNull String destinatario) {
+		publisher.publishEvent(new SaludaEvent(tipo, destinatario));
 	}
 
 	@Override
-	public void saluda(@NonNull String nombre)
-	{
+	public void saluda(@NonNull String nombre) {
 		entorno.write("Hola " + nombre.toUpperCase());
+		onSaluda("saluda", nombre);
+	}
+	
+	public void saluda() {
+		entorno.write("Hola");
+		onSaluda("saluda", "Sin nombre");
 	}
 
 	@Override
 	public int getContador() {
 		return entorno.getContador();
 	}
-	
+
 	public Optional<Entorno> getEntorno() {
 		return Optional.ofNullable(entorno);
 	}

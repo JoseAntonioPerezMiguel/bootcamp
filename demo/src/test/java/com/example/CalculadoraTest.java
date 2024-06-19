108e5519ca216a13dfd8e80d57efe39e3b752917
JoseAntonioPerezMiguel <jose-antonio.perez-miguel@capgemini.com>
2024-06-19 13:21:01 +0200

    Formated Claculadora tests

--- demo/src/test/java/com/example/CalculadoraTest.java
+++ demo/src/test/java/com/example/CalculadoraTest.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Pruebas de la clase calculadora")
 class CalculadoraTest {
 
 	@BeforeAll
@@ -25,34 +28,60 @@ class CalculadoraTest {
 	@AfterEach
 	void tearDown() throws Exception {
 	}
-
-	@Test
-	void testAdd() {
-		var result = Calculadora.add(1, 2);
-		assertEquals(3, result);
+	
+	@Nested
+	class Add {
+		
+		@Nested
+		class OK {
+			@Test
+			@DisplayName("Suma dos enteros")
+			void testAdd() {
+				var result = Calculadora.add(1, 2);
+				assertEquals(3, result);
+			}
+			
+			@Test
+			@DisplayName("Suma dos reales")
+			void testAdd2() {
+				var result = Calculadora.add(1, -0.9);
+				assertEquals(0.1, result);
+			}			
+		}
+		
+		@Nested
+		class KO {
+			
+		}
 	}
 	
-	@Test
-	void testAdd2() {
-		var result = Calculadora.add(1, -0.9);
-		assertEquals(0.1, result);
+	@Nested
+	@DisplayName("Método Div")
+	class Div {		
+		@Nested
+		class OK {
+			@Test
+			@DisplayName("Divide dos enteros")
+			void testDivInt() {
+				var result = Calculadora.div(3, 2);
+				assertEquals(1, result);
+			}
+			
+			@Test
+			@DisplayName("Divide dos reales")
+			void testDivReal() {
+				var result = Calculadora.div(3.0, 2.0);
+				assertEquals(1.5, result);
+			}			
+		}
+		
+		@Nested
+		class KO {
+			@Test
+			@DisplayName("Divide entre 0")
+			void testDivRealKO() {
+				assertThrows(ArithmeticException.class, () -> Calculadora.div(3.0, 0));
+			}
+		}
 	}
-
-	@Test
-	void testDivInt() {
-		var result = Calculadora.div(3, 2);
-		assertEquals(1, result);
-	}
-	
-	@Test
-	void testDivReal() {
-		var result = Calculadora.div(3.0, 2.0);
-		assertEquals(1.5, result);
-	}
-	
-	@Test
-	void testDivRealKO() {
-		assertThrows(ArithmeticException.class, () -> Calculadora.div(3.0, 0));
-	}
-
 }

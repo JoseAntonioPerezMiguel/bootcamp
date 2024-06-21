6cb02528742347e900d4759ebccefafefc019959
JoseAntonioPerezMiguel <jose-antonio.perez-miguel@capgemini.com>
2024-06-21 09:49:37 +0200

    Removing commented old methods

--- glided_rose_kata/src/test/java/com/gildedrose/GildedRoseTest.java
+++ glided_rose_kata/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,5 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -25,100 +24,11 @@ class GildedRoseTest {
 			"'Patata', -1, 1, -2, 0",
 			"'Conjured Mana Cake', 3, 6, 2, 4",
 			"'Conjured Mana Cake', -1, 6, -2, 2"})
-	void test(String name, int sellIn, int quality, int expectedSellIn, int expectedQuality) {
+	void updateQualityTest(String name, int sellIn, int quality, int expectedSellIn, int expectedQuality) {
 		Item[] items = new Item[] { new Item(name, sellIn, quality) };
 		GildedRose app = new GildedRose(items);
 		app.updateQuality();
 		assertEquals(expectedQuality, app.items[0].quality);
 		assertEquals(expectedSellIn, app.items[0].sellIn);
 	}
-	
-
-	@Test
-	void givenAgedBrie_WhenUpdatingQuality_ThenQualityRises() {
-		Item[] items = new Item[] { new Item("Aged Brie", 3, 0) };
-		GildedRose app = new GildedRose(items);
-		app.updateQuality();
-		assertEquals(1, app.items[0].quality);
-	}
-	
-//	@Test
-//	void givenAgedBrieWithSellInZero_WhenUpdatingQuality_ThenQualityDoubleRises() {
-//		Item[] items = new Item[] { new Item("Aged Brie", 0, 0) };
-//		GildedRose app = new GildedRose(items);
-//		app.updateQuality();
-//		assertEquals(2, app.items[0].quality);
-//	}
-//	
-//	@Test
-//	void givenBackstagePassWithSellInTen_WhenUpdatingQuality_ThenQualityDoubleRises() {
-//		Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0) };
-//		GildedRose app = new GildedRose(items);
-//		app.updateQuality();
-//		assertEquals(2, app.items[0].quality);
-//	}
-//	
-//	@Test
-//	void givenBackstagePassWithSellInFive_WhenUpdatingQuality_ThenQualityTripleRises() {
-//		Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0) };
-//		GildedRose app = new GildedRose(items);
-//		app.updateQuality();
-//		assertEquals(3, app.items[0].quality);
-//	}
-//	
-//	@Test
-//	void givenBackstagePassWithSellInZero_WhenUpdatingQuality_ThenQualityDecrementsToZero() {
-//		Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50) };
-//		GildedRose app = new GildedRose(items);
-//		app.updateQuality();
-//		assertEquals(0, app.items[0].quality);
-//	}
-//	
-//	@Test
-//	void givenSulfurasWithTenQuality_WhenUpdatingQuality_ThenQualityStays() {
-//		Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 3, 10) };
-//		GildedRose app = new GildedRose(items);
-//		app.updateQuality();
-//		assertEquals(10, app.items[0].quality);
-//	}
-//	
-//	@Test
-//	void givenItemWith50Quality_WhenUpdatingQuality_ThenQualityDoesntRise() {
-//		Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 3, 50) };
-//		GildedRose app = new GildedRose(items);
-//		app.updateQuality();
-//		assertEquals(50, app.items[0].quality);
-//	}
-//	
-//	@Test
-//	void givenItemWith0Quality_WhenUpdatingQuality_ThenQualityStays() {
-//		Item[] items = new Item[] { new Item("Patata", 3, 0) };
-//		GildedRose app = new GildedRose(items);
-//		app.updateQuality();
-//		assertEquals(0, app.items[0].quality);
-//	}
-//	
-//	@Test
-//	void givenItemWith3Quality_WhenUpdatingQuality_ThenQualityDecrementsOne() {
-//		Item[] items = new Item[] { new Item("Patata", 3, 3) };
-//		GildedRose app = new GildedRose(items);
-//		app.updateQuality();
-//		assertEquals(2, app.items[0].quality);
-//	}
-//	
-//	@Test
-//	void givenItemWith3QualityAndSellinNegative_WhenUpdatingQuality_ThenQualityDecrementsDouble() {
-//		Item[] items = new Item[] { new Item("Patata", -1, 3) };
-//		GildedRose app = new GildedRose(items);
-//		app.updateQuality();
-//		assertEquals(1, app.items[0].quality);
-//	}
-//	
-//	@Test
-//	void givenItemWith1QualityAndSellinNegative_WhenUpdatingQuality_ThenQualityDecrementsDouble() {
-//		Item[] items = new Item[] { new Item("Patata", -1, 1) };
-//		GildedRose app = new GildedRose(items);
-//		app.updateQuality();
-//		assertEquals(0, app.items[0].quality);
-//	}
 }

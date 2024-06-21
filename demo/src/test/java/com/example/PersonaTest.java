fab6a2077630020abb159a9e46ea9fa45462e956
JoseAntonioPerezMiguel <jose-antonio.perez-miguel@capgemini.com>
2024-06-21 12:55:40 +0200

    OK and KO tests for UpperCaseName in PersonService

--- demo/src/test/java/com/example/PersonaTest.java
+++ demo/src/test/java/com/example/PersonaTest.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assumptions.assumeFalse;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
@@ -10,7 +9,6 @@ import java.util.Optional;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
@@ -32,38 +30,32 @@ class PersonaTest {
 				var persona = new Persona(1, "Muis");
 //				var persona = new Persona(2, "Nergio", "Sosti");
 				assertNotNull(persona);
-				assertAll("Persona", 
-						() -> assertEquals(1, persona.getId(), "id"),
+				assertAll("Persona", () -> assertEquals(1, persona.getId(), "id"),
 						() -> assertEquals("Muis", persona.getNombre(), "nombre"),
-						() -> assertTrue(persona.getApellidos().isEmpty(), "apellidos")
-				);
+						() -> assertTrue(persona.getApellidos().isEmpty(), "apellidos"));
 			}
-			
+
 			@ParameterizedTest(name = "{0} {1}")
-			@CsvSource(value = {"1,Muis", "2,'Nergio, Sosti'", "3, Nergio, Sosti"})
+			@CsvSource(value = { "1,Muis", "2,'Nergio, Sosti'", "3, Nergio, Sosti" })
 			void soloNombre(int id, String nombre) {
 				var persona = new Persona(id, nombre);
 				assertNotNull(persona);
-				assertAll("Persona",
-						() -> assertEquals(id, persona.getId(), "id"),
+				assertAll("Persona", () -> assertEquals(id, persona.getId(), "id"),
 						() -> assertEquals(nombre, persona.getNombre(), "nombre"),
-						() -> assertTrue(persona.getApellidos().isEmpty(), "apellidos")
-						);
+						() -> assertTrue(persona.getApellidos().isEmpty(), "apellidos"));
 			}
-			
+
 			@ParameterizedTest(name = "{0} {1}")
-			@CsvSource(value = {"1,Muis", "2,'Nergio, Sosti'", "3, Nergio, Sosti"})
+			@CsvSource(value = { "1,Muis", "2,'Nergio, Sosti'", "3, Nergio, Sosti" })
 			@Disabled
 			void soloNombre(ArgumentsAccessor args) {
-				var persona = args.size() == 3 ? new Persona(args.getInteger(0), args.getString(1), args.getString(2)) :
-					new Persona(args.getInteger(0), args.getString(1));
+				var persona = args.size() == 3 ? new Persona(args.getInteger(0), args.getString(1), args.getString(2))
+						: new Persona(args.getInteger(0), args.getString(1));
 				assertNotNull(persona);
-				assertAll("Persona",
-						() -> assertEquals(args.getInteger(0), persona.getId(), "id"),
+				assertAll("Persona", () -> assertEquals(args.getInteger(0), persona.getId(), "id"),
 						() -> assertEquals(args.getString(1), persona.getNombre(), "nombre"),
-						() -> assertTrue(args.size() == 3 ? persona.getApellidos().isPresent() :
-							persona.getApellidos().isEmpty(), "apellidos")
-						);
+						() -> assertTrue(args.size() == 3 ? persona.getApellidos().isPresent()
+								: persona.getApellidos().isEmpty(), "apellidos"));
 //				assumeFalse(true, "falta los apellidos");
 			}
 		}
@@ -71,20 +63,39 @@ class PersonaTest {
 		@Nested
 		class KO {
 			@ParameterizedTest(name = "{0} {1}")
-			@CsvSource(value = {"3, ''", "5, '      '"})
+			@CsvSource(value = { "3, ''", "5, '      '" })
 			void soloNombre(int id, String nombre) {
 				assertThrows(Exception.class, () -> new Persona(id, nombre));
 			}
 		}
-		
-		@Test
-		void setUpperCaseNameService() {
-			Persona person = new Persona(1, "Pepito", "Grillo");
-			var dao = mock(PersonaRepository.class);
-			when(dao.getOne(anyInt())).thenReturn(Optional.of(person));
-			var service = new PersonService(dao);
-			service.setUpperCaseName(1);
-			verify(dao).modify(person);
+
+		@Nested
+		@DisplayName("UpperCaseName in PersonService")
+		class UpperCaseName {
+			
+			@Nested
+			class OK {
+				@Test
+				void setUpperCaseNameService() {
+					Persona person = new Persona(1, "Pepito", "Grillo");
+					var dao = mock(PersonaRepository.class);
+					when(dao.getOne(anyInt())).thenReturn(Optional.of(person));
+					var service = new PersonService(dao);
+					service.setUpperCaseName(1);
+					verify(dao).modify(person);
+				}
+			}
+			
+			@Nested
+			class KO {
+				@Test
+				void setUpperCaseNameService() {
+					var dao = mock(PersonaRepository.class);
+					when(dao.getOne(anyInt())).thenReturn(Optional.empty());
+					var service = new PersonService(dao);
+					assertThrows(IllegalArgumentException.class, () -> service.setUpperCaseName(1));
+				}
+			}
 		}
 	}
 }

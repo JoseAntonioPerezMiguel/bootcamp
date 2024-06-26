4164491f9b4331efaf22e934d9039bfcb0ba7da4
JoseAntonioPerezMiguel <jose-antonio.perez-miguel@capgemini.com>
2024-06-26 08:54:14 +0200

    Adding PK to Services Ids

--- catalog/src/main/java/com/example/core/contracts/services/FilmActorService.java
+++ catalog/src/main/java/com/example/core/contracts/services/FilmActorService.java
@@ -2,6 +2,7 @@ package com.example.core.contracts.services;
 
 import com.example.domains.core.contracts.services.ProjectionDomainService;
 import com.example.domains.entities.FilmActor;
+import com.example.domains.entities.FilmActorPK;
 
-public interface FilmActorService extends ProjectionDomainService<FilmActor, Integer>{
+public interface FilmActorService extends ProjectionDomainService<FilmActor, FilmActorPK>{
 }
--- catalog/src/main/java/com/example/core/contracts/services/FilmCategoryService.java
+++ catalog/src/main/java/com/example/core/contracts/services/FilmCategoryService.java
@@ -2,6 +2,7 @@ package com.example.core.contracts.services;
 
 import com.example.domains.core.contracts.services.ProjectionDomainService;
 import com.example.domains.entities.FilmCategory;
+import com.example.domains.entities.FilmCategoryPK;
 
-public interface FilmCategoryService extends ProjectionDomainService<FilmCategory, Integer>{
+public interface FilmCategoryService extends ProjectionDomainService<FilmCategory, FilmCategoryPK>{
 }

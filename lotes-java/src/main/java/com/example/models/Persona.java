d883fe64d4b2431ab8f8c72d402ca2f61f38f2a2
JoseAntonioPerezMiguel <jose-antonio.perez-miguel@capgemini.com>
2024-06-27 12:38:06 +0200

    Created entity and its DTO

---
+++ lotes-java/src/main/java/com/example/models/Persona.java
@@ -0,0 +1,11 @@
+package com.example.models;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+	private long id;
+	private String name;
+	private String email;
+	private String ip;
+}
---
+++ lotes-java/src/main/java/com/example/models/PersonaDTO.java
@@ -0,0 +1,13 @@
+package com.example.models;
+
+import lombok.Data;
+
+@Data
+public class PersonaDTO {
+	private long id;
+	private String name;
+	private String surname;
+	private String email;
+	private String gender;
+	private String ip;	
+}

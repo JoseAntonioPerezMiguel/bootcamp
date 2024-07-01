31da0c52813a8081c64cfeddd13d209a8f1fb25f
JoseAntonioPerezMiguel <jose-antonio.perez-miguel@capgemini.com>
2024-07-01 08:46:34 +0200

    Adding subtract and multiply on client

--- demo/src/main/java/com/example/application/proxies/CalculatorProxyImpl.java
+++ demo/src/main/java/com/example/application/proxies/CalculatorProxyImpl.java
@@ -1,15 +1,23 @@
 package com.example.application.proxies;
 
+import java.math.RoundingMode;
+
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 import com.example.domains.contracts.proxies.CalculatorProxy;
 import com.example.webservice.schema.AddRequest;
 import com.example.webservice.schema.AddResponse;
+import com.example.webservice.schema.MultiplyRequest;
+import com.example.webservice.schema.MultiplyResponse;
+import com.example.webservice.schema.SubtractRequest;
+import com.example.webservice.schema.SubtractResponse;
 
 public class CalculatorProxyImpl extends WebServiceGatewaySupport implements CalculatorProxy {
+	
+	private static final int SCALE = 14;
+	
 	public double add(double a, double b) {
-		return b;
 		var request = new AddRequest();
 		request.setOp1(a);
 		request.setOp2(b);
@@ -18,4 +26,25 @@ public class CalculatorProxyImpl extends WebServiceGatewaySupport implements Cal
 		return response.getAddResult();
 	}
 
+	public double subtract(double a, double b) {
+		var request = new SubtractRequest();
+		request.setOp1(a);
+		request.setOp2(b);
+		var response = (SubtractResponse) getWebServiceTemplate().marshalSendAndReceive(request,
+				new SoapActionCallback("http://example.com/webservices/schemas/calculator"));
+		return response.getSubtractResult();
+	}
+	
+	public double multiply(double a, double b) {
+		var request = new MultiplyRequest();
+		request.setOp1(a);
+		request.setOp2(b);
+		var response = (MultiplyResponse) getWebServiceTemplate().marshalSendAndReceive(request,
+				new SoapActionCallback("http://example.com/webservices/schemas/calculator"));
+		return response.getMultiplyResult();
+	}
+	
+	private double round(double value) {
+		return BigDecimal.valueOf(value).setScale(SCALE, RoundingMode.HALF_UP).doubleValue();
+	}
 }

fd77cc031704f2e2746eac2db91b5f9bb460da5f
JoseAntonioPerezMiguel <jose-antonio.perez-miguel@capgemini.com>
2024-07-02 09:56:58 +0200

    Little change on sql query

--- catalog/src/main/java/com/example/domains/contracts/repositories/FilmRepository.java
+++ catalog/src/main/java/com/example/domains/contracts/repositories/FilmRepository.java
@@ -10,8 +10,7 @@ import java.util.List;
 
 public interface FilmRepository extends JpaRepository<Film, Integer>, RepositoryWithProjections{
 	
-	@Query(value = "SELECT f.*"
-			+ "FROM film f JOIN film_category fc ON f.film_id = fc.film_id WHERE fc.category_id = ?1", nativeQuery = true)
+	@Query(value = "SELECT f.* FROM film f JOIN film_category fc ON f.film_id = fc.film_id WHERE fc.category_id = :categoryId", nativeQuery = true)
 	List<Film> findByCategory(int categoryId);
 
 	@Query("from Film f join f.language l where l.languageId = :id")

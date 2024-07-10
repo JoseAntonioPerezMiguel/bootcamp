744bf31c8c51e64d61dcb834775ac7496dd57b02
JoseAntonioPerezMiguel <jose-antonio.perez-miguel@capgemini.com>
2024-07-10 13:37:53 +0200

    Logger

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SecurityModule } from './security';
+import { LoggerService, MyCoreModule } from '@my/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SecurityModule, MyCoreModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'curso';
+  
+  constructor(log: LoggerService) {
+    log.error('Es un error')
+    log.warn('Es un warn')
+    log.info('Es un info')
+    log.log('Es un log')
+  }
 }

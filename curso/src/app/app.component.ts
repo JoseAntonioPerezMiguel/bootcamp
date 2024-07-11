850d9810f257eca8f6a3e423d3b3cd20c1bc85ef
JoseAntonioPerezMiguel <jose-antonio.perez-miguel@capgemini.com>
2024-07-11 09:30:11 +0200

    Home component

--- curso/src/app/app.component.ts
+++ curso/src/app/app.component.ts
@@ -1,25 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SecurityModule } from './security';
-import { LoggerService, MyCoreModule } from '@my/core';
+import { /*LoggerService,*/ MyCoreModule } from '@my/core';
 // import { NotificationComponent } from './main/notification/notification.component';
 import { DemosComponent } from './demos/demos.component';
 import { NotificationModalComponent } from './main';
+import { HomeComponent } from "./main/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SecurityModule, MyCoreModule, NotificationModalComponent, DemosComponent,],
+  imports: [RouterOutlet, SecurityModule, MyCoreModule, NotificationModalComponent, DemosComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'curso';
   
-  constructor(log: LoggerService) {
-    log.error('Es un error')
-    log.warn('Es un warn')
-    log.info('Es un info')
-    log.log('Es un log')
-  }
+  // constructor(log: LoggerService) {
+  //   log.error('Es un error')
+  //   log.warn('Es un warn')
+  //   log.info('Es un info')
+  //   log.log('Es un log')
+  // }
 }

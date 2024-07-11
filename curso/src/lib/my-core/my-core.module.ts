bdd44875b19faa5ad1cfb40cc097723a8329e5b9
JoseAntonioPerezMiguel <jose-antonio.perez-miguel@capgemini.com>
2024-07-11 14:34:01 +0200

    Pipes and style

--- curso/src/lib/my-core/my-core.module.ts
+++ curso/src/lib/my-core/my-core.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PIPES_STRINGS } from './pipes/strings.pipe';
 
 
 
 @NgModule({
   declarations: [],
-  exports: [],
+  exports: [ PIPES_STRINGS],
   imports: [
-    CommonModule
+    CommonModule, PIPES_STRINGS
   ]
 })
 export class MyCoreModule { }

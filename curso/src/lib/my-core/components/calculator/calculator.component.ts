220639aabb252b6a36e442e528a7c2188bd8dcd5
JoseAntonioPerezMiguel <jose-antonio.perez-miguel@capgemini.com>
2024-07-12 12:01:08 +0200

    calculator working

--- curso/src/lib/my-core/components/calculator/calculator.component.ts
+++ curso/src/lib/my-core/components/calculator/calculator.component.ts
@@ -2,11 +2,75 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
-  standalone: true,
-  imports: [],
   templateUrl: './calculator.component.html',
-  styleUrl: './calculator.component.css'
+  styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent {
+  display: string = '';
+  
+  operandos = ['', '']
+  operator = ''
 
+  addNumber(op: number) {
+    if (this.operator === '') {
+      this.operandos[0] += op.toString();
+      this.display = this.operandos[0];
+    } else {
+      this.operandos[1] += op.toString();
+      this.display += op.toString();
+    }
+  }
+
+  addComma() {
+    if (this.operator === '') {
+      if (!this.operandos[0].includes('.')) {
+        this.operandos[0] += '.';
+        this.display = this.operandos[0];
+      }
+    } else {
+      if (!this.operandos[1].includes('.')) {
+        this.operandos[1] += '.';
+        this.display += '.';
+      }
+    }
+  }
+
+  addOperator(operator: string) {
+    if (this.operator !== '' && this.operandos[1] !== '') {
+      this.operandos[0] = this.calculate().toString();
+      this.operandos[1] = '';
+      this.display = this.operandos[0] + operator;
+    } else {
+      this.display += operator;
+    }
+    this.operator = operator;
+  }
+
+  calculate(): number {
+    const num1 = parseFloat(this.operandos[0]);
+    const num2 = parseFloat(this.operandos[1]);
+    switch (this.operator) {
+      case "+":
+        return num1 + num2
+      case "-":
+        return num1 - num2
+      case "*":
+        return num1 * num2
+      case "/" :
+        return num1 / num2
+      default:
+        return NaN
+    }
+  }
+
+  calculateButton() {
+    this.display = this.calculate().toString()
+  }
+
+  clear() {
+    this.display = '';
+    this.operandos[0] = ''
+    this.operandos[1] = ''
+    this.operator = ''
+  }
 }
